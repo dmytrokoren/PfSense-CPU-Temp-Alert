9877d018dd202ab6ce38b212d7d6e94e99836b20
Dmytro Korenkov <dmytrokoren@gmail.com>
2024-03-06 01:58:57 -0600

    tt

--- PfCPU_temp_alert.sh
+++ PfCPU_temp_alert.sh
@@ -44,7 +44,6 @@ for i in $(seq 1 1 $iterations); do
                 #wall pftemp_alert.txt
                 #rm pftemp_alert.txt
                 php -r 'require_once("/etc/inc/notices.inc"); notify_via_telegram("\u{203C}\u{FE0F} High temp warning: '${avg}'\u{2103} \u{203C}\u{FE0F}");'
-                curl -fsS -m 120 --data-raw "WARNING: ${thishost} is currently at ${avg}C, which is over the alert threshold of ${alert}C" https://hc-ping.com/33c45928-c63e-42de-9f74-fbaa621353f6/$?
         fi
 
         sleep $timeInSeconds
@@ -52,4 +51,4 @@ done
 
 exit 0
 
-curl -fsS -m 120 --data-raw "Current temp: $avg" https://hc-ping.com/33c45928-c63e-42de-9f74-fbaa621353f6/$?
+curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/33c45928-c63e-42de-9f74-fbaa621353f6/$?
